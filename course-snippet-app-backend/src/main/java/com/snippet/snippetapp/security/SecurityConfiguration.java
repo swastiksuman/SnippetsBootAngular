991b7f042ff1d874db4a3629914f581d895aff24
Swastik Panda <swastiksuman@gmail.com>
2019-05-01 21:33:17 +0530

    oauth added

--- course-snippet-app-backend/src/main/java/com/snippet/snippetapp/security/SecurityConfiguration.java
+++ course-snippet-app-backend/src/main/java/com/snippet/snippetapp/security/SecurityConfiguration.java
@@ -1,16 +1,20 @@
 package com.snippet.snippetapp.security;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import com.auth0.spring.security.api.JwtWebSecurityConfigurer;
+
 
 @Configuration
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 	
-	@Override
+	/*@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 		auth.inMemoryAuthentication().withUser("user").password("{noop}password").roles("USER").and().withUser("admin")
 				.password("{noop}admin").roles("USER", "ADMIN");
@@ -24,5 +28,25 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 	    .antMatcher("/login").authorizeRequests().and()
 	      .authorizeRequests()
 	        .anyRequest().authenticated();
-	  }
+	  }*/
+
+	@Value(value = "${auth0.apiAudience}")
+	private String apiAudience;
+	@Value(value = "${auth0.issuer}")
+	private String issuer;
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		JwtWebSecurityConfigurer
+				.forRS256(apiAudience, issuer)
+				.configure(http)
+				.authorizeRequests()
+				.antMatchers(HttpMethod.GET, "/*.js").permitAll()
+				.antMatchers(HttpMethod.GET, "/").permitAll()
+				.antMatchers(HttpMethod.GET, "/snippets/allsnippets").permitAll()
+				.antMatchers(HttpMethod.GET, "/api/v1/bikes").hasAuthority("view:registrations")
+				.antMatchers(HttpMethod.GET, "/api/v1/bikes/**").hasAuthority("view:registration")
+				.anyRequest().authenticated();
+	}
+
 }
